a4793dfd9c6700da9e5d48ffe3de09ff75d4d3ab
SachinVerma09 <92529240+SachinVerma09@users.noreply.github.com>
2021-11-15 00:15:44 +0530

    initial push

---
+++ index.js
@@ -0,0 +1,78 @@
+'use strict';
+
+
+var os = require('os');
+var express = require('express');
+var app = express();
+var http = require('http');
+
+var socketIO = require('socket.io');
+
+
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
+	res.render("index.ejs");
+});
+
+var server = http.createServer(app);
+
+server.listen(process.env.PORT || 8000);
+
+var io = socketIO(server);
+
+io.sockets.on('connection', function(socket) {
+
+
+	function log() {
+	  var array = ['Message from server:'];
+	  array.push.apply(array, arguments);
+	  socket.emit('log', array);
+	}
+  
+    
+    socket.on('message', function(message, room) {
+	  log('Client said: ', message);
+	  
+	  socket.in(room).emit('message', message, room);
+	});
+  
+	socket.on('create or join', function(room) {
+	  log('Received request to create or join room ' + room);
+  
+	  var clientsInRoom = io.sockets.adapter.rooms[room];
+	  var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+	  log('Room ' + room + ' now has ' + numClients + ' client(s)');
+  
+	  if (numClients === 0) {
+		socket.join(room);
+		log('Client ID ' + socket.id + ' created room ' + room);
+		socket.emit('created', room, socket.id);
+  
+	  } else if (numClients === 1) {
+		log('Client ID ' + socket.id + ' joined room ' + room);
+		io.sockets.in(room).emit('join', room);
+		socket.join(room);
+		socket.emit('joined', room, socket.id);
+		io.sockets.in(room).emit('ready');
+	  } else { 
+		socket.emit('full', room);
+	  }
+	});
+  
+	socket.on('ipaddr', function() {
+	  var ifaces = os.networkInterfaces();
+	  for (var dev in ifaces) {
+		ifaces[dev].forEach(function(details) {
+		  if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+			socket.emit('ipaddr', details.address);
+		  }
+		});
+	  }
+	});
+  
+	socket.on('bye', function(){
+	  console.log('received bye');
+	});
+  
+  });
